b0e1fe36f2e563695368b7b294f0a4bca7f45fd7
calm0815 <calm0815@example.com>
2019-08-30 16:34:04 +0900

    graph output

--- src/graph_plot.py
+++ src/graph_plot.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def quat2euler(quaternion):
-    euler = tf.transformations.quat2euler((quaternion.x, quaternion.y, quaternion.z, quaternion.w))
+    euler = tf.transformations.euler_from_quaternion([quaternion.x, quaternion.y, quaternion.z, quaternion.w])
     return Vector3(x=euler[0], y=euler[1], z=euler[2])  # RPY
 
 if __name__ == '__main__':
@@ -19,12 +19,9 @@ if __name__ == '__main__':
     tfBuffer = tf2_ros.Buffer()
     listener = tf2_ros.TransformListener(tfBuffer)
     
-    flip=1
-    x = np.linspace(0, 14, 100)
-    for i in range(1, 7):
-        plt.plot(x, np.sin(x + i * .5) * (7 - i) * flip)
-
-    plt.show()
+    ax=plt.subplot(projection="polar")
+    r = [1, 1, 1, 1]
+    theta = [0, 0, 0, 0]
 
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
@@ -32,10 +29,24 @@ if __name__ == '__main__':
             trans_A = tfBuffer.lookup_transform('world', 'TF_A', rospy.Time())
             trans_B = tfBuffer.lookup_transform('world', 'TF_B', rospy.Time())
             trans_C = tfBuffer.lookup_transform('world', 'TF_C', rospy.Time())
+            trans_ave = tfBuffer.lookup_transform('world', 'TF_average', rospy.Time())
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
             rate.sleep()
             continue
+        
+        ax.cla()
+        theta = [quat2euler(trans_A.transform.rotation).y, 
+                 quat2euler(trans_B.transform.rotation).y, 
+                 quat2euler(trans_C.transform.rotation).y, 
+                 quat2euler(trans_ave.transform.rotation).y]
+        
+        ax.scatter(theta[0], r[0], color='b')
+        ax.scatter(theta[1], r[1], color='g')
+        ax.scatter(theta[2], r[2], color='c')
+        ax.scatter(theta[3], r[3], color='r')
+        ax.set_rlim((0, 1.5))
+        plt.pause(.01)
 
-        rospy.loginfo(trans_A)
+        rospy.loginfo(quat2euler(trans_A.transform.rotation).y)
 
         rate.sleep()
